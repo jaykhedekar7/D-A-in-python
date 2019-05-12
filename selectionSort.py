e35c53f7e19b4f62a64fc217e340de3cf5cd803e
jaykhedekar7 <jaykhedekar@hotmail.com>
2019-05-12 20:28:27 +0530

    Done

--- selectionSort.py
+++ selectionSort.py
@@ -1,9 +1,11 @@
 def SelectionSort(array):
     length = len(array)
+    for i in range(len(array)): 
+        min_idx = i 
+        for j in range(i+1, len(array)): 
+            if array[min_idx] > array[j]: 
+                min_idx = j 
+                      
+        array[i], array[min_idx] = array[min_idx], array[i] 
 
-    for i in range(length-1):
-        small = array[i]
-
-        for j in range[i+1, length-1]:
-            if array[small] > array[j]:
-                array[small], array[j] = array[j], array[small]
+    return array       
